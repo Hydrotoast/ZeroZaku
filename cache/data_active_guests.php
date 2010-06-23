3003f999a9e4c6b71808dc12842ec8bf535449d6
Gio Cielo <gio.cielo@zerozaku.com>
2010-06-22 22:55:18 -0700

    added the modules based on friends module

--- cache/data_active_guests.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1276842730
-214
-a:2:{s:32:"8d4954a3bdbeba7b08bf1837c9802ef2";a:1:{s:10:"session_id";s:32:"8d4954a3bdbeba7b08bf1837c9802ef2";}s:32:"fb194e3a7851ee42e133d6a48bdab646";a:1:{s:10:"session_id";s:32:"fb194e3a7851ee42e133d6a48bdab646";}}
