11cc744cfe4eb99c3af9c961f46e232cb1172bd5
Gio Borje <killaziller@yahoo.com>
2010-06-28 23:02:50 -0700

    restyled sidebar modules and removed the bottom border from the first row of each category/forum

--- cache/tpl_zerozaku_instant_messenger_online_body.html.php
+++ cache/tpl_zerozaku_instant_messenger_online_body.html.php
@@ -1,3 +1,3 @@
 <?php if (!defined('IN_PHPBB')) exit; if (sizeof($this->_tpldata['online_row'])) {  $_online_row_count = (isset($this->_tpldata['online_row'])) ? sizeof($this->_tpldata['online_row']) : 0;if ($_online_row_count) {for ($_online_row_i = 0; $_online_row_i < $_online_row_count; ++$_online_row_i){$_online_row_val = &$this->_tpldata['online_row'][$_online_row_i]; ?>
-		<li><a href="#" userid="<?php echo $_online_row_val['USER_ID']; ?>"><span style="background-image: url('<?php echo $_online_row_val['IMG_STATUS']; ?>');"><?php echo $_online_row_val['AVATAR']; ?> <?php echo $_online_row_val['USERNAME']; ?> </a></li>
+		<li><a href="#" userid="<?php echo $_online_row_val['USER_ID']; ?>" title="<?php echo $_online_row_val['USERNAME']; ?>"><span style="background-image: url('<?php echo $_online_row_val['IMG_STATUS']; ?>');"><?php echo $_online_row_val['AVATAR']; ?> <?php echo $_online_row_val['USERNAME']; ?> </a></li>
 	<?php }} } ?>
--- cache/tpl_zerozaku_milestones.html.php
+++ cache/tpl_zerozaku_milestones.html.php
@@ -1,17 +1,4 @@
-<?php if (!defined('IN_PHPBB')) exit; if ($this->_rootref['S_KEYWORD_VERSION']) {  ?>
-<!--
-/**
-*
-* @package - "Milestone Congratulations"
-* @version $Id: milestones.html 3 2008-09-21 MartectX $
-* @copyright (C)2008, MartectX ( http://mods.martectx.de/ )
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
-*
-*/
--->
-<?php } ?>
-
-<?php echo (isset($this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_L'])) ? $this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_L'] : ''; ?><img src="<?php echo (isset($this->_rootref['T_THEME_PATH'])) ? $this->_rootref['T_THEME_PATH'] : ''; ?>/images/globe.png" width="16px" height="16px" alt=""/>&nbsp;<?php echo ((isset($this->_rootref['L_MILESTONE_HISTORY'])) ? $this->_rootref['L_MILESTONE_HISTORY'] : ((isset($user->lang['MILESTONE_HISTORY'])) ? $user->lang['MILESTONE_HISTORY'] : '{ MILESTONE_HISTORY }')); echo (isset($this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_R'])) ? $this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_R'] : ''; ?>
+<?php if (!defined('IN_PHPBB')) exit; echo (isset($this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_L'])) ? $this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_L'] : ''; ?><img src="<?php echo (isset($this->_rootref['T_THEME_PATH'])) ? $this->_rootref['T_THEME_PATH'] : ''; ?>/images/globe.png" width="16px" height="16px" alt=""/>&nbsp;<?php echo ((isset($this->_rootref['L_MILESTONE_HISTORY'])) ? $this->_rootref['L_MILESTONE_HISTORY'] : ((isset($user->lang['MILESTONE_HISTORY'])) ? $user->lang['MILESTONE_HISTORY'] : '{ MILESTONE_HISTORY }')); echo (isset($this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_R'])) ? $this->_tpldata['DEFINE']['.']['TBL_BLOCK_H_R'] : ''; ?>
 
 <?php $_milestone_count = (isset($this->_tpldata['milestone'])) ? sizeof($this->_tpldata['milestone']) : 0;if ($_milestone_count) {for ($_milestone_i = 0; $_milestone_i < $_milestone_count; ++$_milestone_i){$_milestone_val = &$this->_tpldata['milestone'][$_milestone_i]; if ($_milestone_val['NEWTYPE']) {  if (! $_milestone_val['S_FIRST_ROW']) {  ?>
 	</tbody>
@@ -23,15 +10,15 @@
 	<table class="table1" cellspacing="1">
 	<thead>
 		<tr>
-			<th class="active" style="width: 30%; text-align: right;"><?php echo $_milestone_val['TYPE']; ?></th>
 			<th class="name" style="text-align: left;"><?php echo ((isset($this->_rootref['L_USERNAME'])) ? $this->_rootref['L_USERNAME'] : ((isset($user->lang['USERNAME'])) ? $user->lang['USERNAME'] : '{ USERNAME }')); ?></th>
+			<th class="active" style="width: 10%; text-align: center;"><?php echo $_milestone_val['TYPE']; ?></th>
 		</tr>
 	</thead>
 	<tbody>
 <?php } ?>
 		<tr class="<?php if (($_milestone_val['S_ROW_COUNT'] & 1)  ) {  ?>bg1<?php } else { ?>bg2<?php } ?>">
-			<td class="active" style="width: 30%; text-align: right;"><?php echo $_milestone_val['NUMBER']; ?></td>
 			<td><?php echo $_milestone_val['USER']; ?></td>
+			<td class="active" style="width: 10%; text-align: center;"><?php echo $_milestone_val['NUMBER']; ?></td>
 		</tr>
 <?php }} ?>
 	</tbody>
