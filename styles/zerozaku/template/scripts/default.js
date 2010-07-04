162ea795a0899241c3e10c6733ec0f78ec3f4667
Gio Cielo <gio.cielo@zerozaku.com>
2010-07-04 11:48:31 -0700

    further reduced http requests

--- styles/zerozaku/template/scripts/default.js
+++ styles/zerozaku/template/scripts/default.js
@@ -1,13 +1,3 @@
-/* Copyright (c) 2009 Jon Rohan (http://dinnermint.org)
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- *
- * Version: 1.0.0
- * Written with jQuery 1.3.2
- */
-(function($){$.fn.growing=function(options){var settings=$.extend({maxHeight:400,minHeight:40,buffer:0},options);return this.each(function(){var textarea=$(this);var minh=textarea.height()>settings.minHeight?textarea.height():settings.minHeight;var w=parseInt(textarea.width()||textarea.css("width"));var div=$("<div class='faketextarea' style='position:absolute;left:-10000px;width:"+w+"px;'></div>");textarea.after(div);var resizeBox=function(){var html=textarea.val().replace(/(<|>)/g,'').replace(/\n/g,"<br>|");if(html!=div.html()){div.html(html);var h=div.height();prevh=textarea.height();var newh=h<=minh?minh:(h>settings.maxHeight?settings.maxHeight:h);newh+=settings.buffer;if(newh>=settings.maxHeight){textarea.css("overflow","auto")}else{textarea.css("overflow","hidden")}textarea.css({"height":newh+"px"})}};textarea.keydown(resizeBox);textarea.keyup(resizeBox);resizeBox()})}})(jQuery);
-
-
 /* ZeroZaku Initialization
  * Copyright (c) 2009, 2010 Gio Borje (http://www.zerozaku.com)
  */
