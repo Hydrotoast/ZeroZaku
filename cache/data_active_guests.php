ba6cb490226e25188bbc3239b6347d798bb888ac
Gio Cielo <gio.cielo@zerozaku.com>
2010-07-04 23:03:35 -0700

    added pixel outline for headers, added headers for registration page and added table styling to memberlist

--- cache/data_active_guests.php
+++ cache/data_active_guests.php
@@ -1,4 +1,4 @@
 <?php exit; ?>
-1278274655
-6
-a:0:{}
+1278312501
+214
+a:2:{s:32:"a0a69db70bf9a9b072e11d2ba606cacb";a:1:{s:10:"session_id";s:32:"a0a69db70bf9a9b072e11d2ba606cacb";}s:32:"61319fd1efa80a9fffa986a4a21086f3";a:1:{s:10:"session_id";s:32:"61319fd1efa80a9fffa986a4a21086f3";}}
