a308d6530c47e544d8cd916c3eaefb254bf77caf
Gio Borje <killaziller@yahoo.com>
2010-08-08 16:42:33 -0700

    Hotfix for factions
    Revert "Add form key to CSS override"
    
    This reverts commit 508f4026b5299181c1b92eb87f03323b8073e068.
    
    Add faction pages
    
    Add neutral score color

--- factions.php
+++ factions.php
@@ -1,34 +1,21 @@
 <?php
-##############################################################
-# FILENAME  : factions.php
-# COPYRIGHT : (c) 2008, Tyler N. King <aibotca@yahoo.ca>
-# http://opensource.org/licenses/gpl-license.php GNU Public License
-##############################################################
-define ( 'IN_PHPBB', true );
-$phpbb_root_path	= ( defined ( 'PHPBB_ROOT_PATH' ) ) ? PHPBB_ROOT_PATH : './';
-$phpEx				= substr ( strrchr ( __FILE__, '.' ), 1 );
-include ( $phpbb_root_path . 'common.' . $phpEx );
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include ($phpbb_root_path . 'common.' . $phpEx);
 
 // Initilize the phpBB sessions.
-$user->session_begin ( );
-$auth->acl ( $user->data );
-$user->setup ( 'mods/lang_factions' );
-
-// Include Factions' classes.
-include ( $phpbb_root_path . 'includes/functions_user.' . $phpEx );
-include ( $phpbb_root_path . 'includes/functions_privmsgs.' . $phpEx );
-include ( $phpbb_root_path . 'factions/classes/class_group.' . $phpEx );
-include ( $phpbb_root_path . 'factions/classes/class_tournament.' . $phpEx );
-include ( $phpbb_root_path . 'factions/classes/class_ladder.' . $phpEx );
-include ( $phpbb_root_path . 'factions/functions.' . $phpEx );
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('mods/lang_factions');
 
 // Start the output.
-$action	= request_var ( 'action', '' );
-page_header ( $user->lang[ 'FACTIONS_TITLE' ] );
+$action	= request_var('action', '');
+page_header($user->lang['FACTIONS_TITLE']);
 
 // Include the file for the action called.
-$basename	= basename ( $action, '.' . $phpEx );
-include ( $phpbb_root_path . 'factions/' . $basename . '.' . $phpEx );
+$basename = basename($action . '.' . $phpEx);
+include($phpbb_root_path . 'factions/' . $basename . '.' . $phpEx);
 
-page_footer ( );
+page_footer();
 ?>
