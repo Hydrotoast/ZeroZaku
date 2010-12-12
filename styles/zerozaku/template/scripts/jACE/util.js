22f9b0f9fb5df82677a00ced9295a888de26b3db
Gio Borje <killaziller@yahoo.com>
2010-12-12 15:34:26 -0800

    Create functional prototype of jACE; refs #26
    Add core jace php file
    
    Add jace body template and linked to in core file
    
    Create node, movement, and chat systems
    
    Add message qeue for node messages
    
    Add all new jACE files; functional prototype complete
    
    Commented code a bit

---
+++ styles/zerozaku/template/scripts/jACE/util.js
@@ -0,0 +1,52 @@
+function Queue(nodeLength) {
+	this.nodes = [ null, null, null ];
+	this.head = 0;
+	this.tail = 0;
+}
+
+Queue.prototype = {
+	dequeue : function() {
+		var node = null;
+		if (this.head !== this.tail) {
+			node = this.nodes[this.head++];
+			this.nodes[this.head - 1] = null;
+		}
+		this.head %= this.nodes.length;
+		return node;
+	},
+	enqueue : function(item) {
+		this.nodes[this.tail++] = item;
+		this.tail %= this.nodes.length;
+	},
+	peek : function() {
+		return this.nodes[this.head];
+	},
+	empty : function() {
+		return this.head === this.tail;
+	},
+	full : function() {
+		return this.tail + 1 === this.head;
+	}
+};
+
+function hcf(a, b){
+    var gcd = 1;
+    if (a > b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    if ((b == (Math.round(b / a)) * a)) {
+        gcd = a
+    }
+    else {
+        for (var i = Math.round(a / 2); i > 1; i = i - 1) {
+            if ((a == (Math.round(a / i)) * i)) 
+                if ((b == (Math.round(b / i)) * i)) {
+                    gcd = i;
+                    i = -1;
+                }
+        }
+    }
+    return gcd;
+}
