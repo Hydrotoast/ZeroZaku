9f84a35eaf7d57370df802adabe6900f172422c6
Gio Borje <gio.cielo@zerozaku.com>
2011-05-15 20:29:49 -0700

    Fix topic_terms on posting.php; refs #120 @1h

--- posting.php
+++ posting.php
@@ -1400,7 +1400,7 @@ if ($submit || $preview || $refresh)
 				'topic_last_post_id'	=> (isset($post_data['topic_last_post_id'])) ? (int) $post_data['topic_last_post_id'] : 0,
 				'topic_time_limit'		=> (int) $post_data['topic_time_limit'],
 				'topic_attachment'		=> (isset($post_data['topic_attachment'])) ? (int) $post_data['topic_attachment'] : 0,
-			    'topic_terms'			=> $post_data['topic_terms'],
+			    'topic_terms'			=> (isset($post_data['topic_terms']) ? $post_data['topic_terms'] : array(),
 				'post_id'				=> (int) $post_id,
 				'topic_id'				=> (int) $topic_id,
 				'forum_id'				=> (int) $forum_id,
